dd41bc1e36a2bf25ded9914bed3c9a79912ea420
黃懷萱 <56072039+Chrouos@users.noreply.github.com>
2024-05-15 22:21:36 +0800

    update advisor

--- components/Experience.js
+++ components/Experience.js
@@ -1,11 +1,11 @@
 import Link from '@/components/Link'
 
-const Experience = ({ title, company, location, range, url, text1, text2, text3 }) => {
+const Experience = ({ title, company, location, range, url, textArray = [] }) => {
   return (
     <div className="my-3">
       <div className="flex flex-row text-xl">
         <span className="text-gray-500 dark:text-gray-400">{title}</span>{' '}
-        <span className="text-gray-500 dark:text-gray-400">&nbsp;@&nbsp;</span>{' '}
+        <span className="text-gray-500 dark:text-gray-400">&nbsp;&nbsp;</span>{' '}
         <span className="text-primary-color-500">
           <Link href={url} className="company">
             {company}
@@ -15,22 +15,25 @@ const Experience = ({ title, company, location, range, url, text1, text2, text3 
       <div>
         <div className="p-1 font-mono text-sm text-gray-400 dark:text-gray-600">{range}</div>
         <div className="p-2">
-          <div className="flex flex-row ">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text1}</div>
-          </div>
-          <div className="flex flex-row">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text2}</div>
-          </div>
-          <div className="items-top flex flex-row">
-            <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
-            <div className="text-gray-500 dark:text-gray-400">{text3}</div>
-          </div>
+          {textArray.map((text, index) => (
+            <div key={index} className="items-top flex flex-row">
+              <div className="mr-2 text-lg text-primary-color-500"> &#8227;</div>
+              <div className="text-gray-500 dark:text-gray-400 mr-4 whitespace-nowrap">{text.time}</div>
+              <div className="text-gray-500 dark:text-gray-400 mr-4">
+                {text.content && text.content.map((contentItem, contentIndex) => (
+                  contentItem.link ? (
+                    <a key={contentIndex} href={contentItem.link} target="_blank" rel="noopener noreferrer" className="text-primary-color-500"> {contentItem.text}</a>
+                  ) : (
+                    <a key={contentIndex}>{contentItem.text}</a>
+                  )
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <div className="justify-center text-center text-2xl font-medium text-gray-200  dark:text-gray-600">
-        &#126;&#126;&#126;
+        {/* &#126;&#126;&#126; */}
       </div>
     </div>
   )
--- data/experienceData.js
+++ data/experienceData.js
@@ -1,29 +1,230 @@
 const experienceData = [
   {
-    title: 'Professor',
-    company: 'National Central University',
-    location: 'Taiwan, Taoyuan City',
-    range: 'January 2022 - May 2022',
-    url: 'https://www.accenture.com/gr-en/about/consulting-index',
-    text1:
-      'Helped with the digital transformation of BOAT by working on their platform called ByteETL.',
-    text2:
-      'Built the encryption component for the platform using Scala and Spark which included 2 encoding functionalities : ENCRYPT and HASH.',
-    text3:
-      'Created JSON wrappers for the encryption components and automated and triggered the pipelines to run at specific intervals using CronJob.',
+    title: "Experiences",
+    textArray: [
+      {
+        time: "2024.1 - 2026",
+        content: [
+          { text: "Convenor of Intelligent Computing, NSTC Engineering and Technology Office"},
+          { text: "國科會工程處智慧計算學門召集人", link: "https://www.nstc.gov.tw/eng/ch/detail/58a1eb4d-ce55-4f72-9d59-d35c81f1b5b3"}
+        ]
+      },
+      {
+        time: "2020.2 - Now",
+        content: [
+          { text: "Director of Institutional Research Center ("},
+          { text: "中央大學校務研究辦公室", link: "https://ir.ncu.edu.tw/index.php/zh/"},
+          { text: "主任)"},
+        ]
+      },
+      {
+        time: "2019.7 - 2020.6",
+        content: [
+          { text: "Visiting Faculty at Neural Science Department, UCSD （加州大學聖地牙哥分校）"},
+        ]
+      },
+      {
+        time: "2011.8 - Now",
+        content: [
+          { text: "Professor at CSIE department, NCU (中央大學資工系教授）"},
+        ]
+      },
+      {
+        time: "2014.8 - 2017.7",
+        content: [
+          { text: "Director of CSIE department, NCU (中央大學資工系主任）"},
+        ]
+      },
+      {
+        time: "2007.8 - 2008.7",
+        content: [
+          { text: "Visiting Faculty at"},
+          { text: "Machine Learning Dept", link: "https://www.ml.cmu.edu/"},
+          { text: ", "},
+          { text: "Carnegie Mellon University", link: "https://www.cmu.edu/"},
+          { text: "（卡內基美濃機器學習系訪問學者）"},
+        ]
+      },
+      {
+        time: "2004.8 - 2010.7",
+        content: [
+          { text: "Associate Professor at CSIE, NCU, （中央大學資工系副教授）"},
+        ]
+      },
+      {
+        time: "1999.8 - 2004.7",
+        content: [
+          { text: "Assistant Professor at CSIE, NCU, （中央大學資工系助理教授）"},
+        ]
+      },
+      {
+        time: "1999.2 - 1999.7",
+        content: [
+          { text: "Postdoc at"},
+          { text: "Institue of Information Science", link: "https://www.iis.sinica.edu.tw/zh/index.html"},
+          { text: ", "},
+          { text: "Academic Sinica", link: "https://www.sinica.edu.tw/"},
+          { text: "（中央研究院博士後）"},
+        ]
+      },
+    ]
   },
   {
-    title: 'Software Engineer Analytics',
-    company: 'HOPS Healthcare',
-    location: 'Ahmedabad, India',
-    range: 'March 2021 - June 2021',
-    url: 'https://hops.healthcare/',
-    text1:
-      'Guided by Mr. Vivek Patel, to create a pipeline that extracts essential information from informal conversations.',
-    text2:
-      'Worked on creating a web app for their healthcare system for doctors to analyze and save reports.',
-    text3:
-      'Developed functions for automatically extracting important elements from reports using BioBERT and Regex.',
+    title: "Services",
+    textArray: [
+      {
+        time: "2024 - Now",
+        content: [
+          { text: "Director, Taiwan Associtation for Institutional Research, TAIR ("},
+          { text: "台灣校務研究專業協會", link: "https://tair.tw/"},
+          { text: "理事)"},
+        ]
+      },
+      {
+        time: "2023 - Now",
+        content: [
+          { text: "Supervisor, Association for Computational Linguistic and Chinese Language Processing, ACLCLP ("},
+          { text: "中華民國計算語言學會", link: "http://www.aclclp.org.tw/aclclp_organ.php"},
+          { text: "監事)"},
+        ]
+      },
+      {
+        time: "2023 - Now",
+        content: [
+          { text: "Supervisor, Taiwan Association for Artificial Intelligence, TAAI (第十五屆 "},
+          { text: "中華民國人工智慧學會", link: "https://www.taai.org.tw/"},
+          { text: "監事)"},
+        ]
+      },
+      {
+        time: "2021 - 2022",
+        content: [
+          { text: "Supervisor, Association for Computational Linguistic and Chinese Language Processing, ACLCLP ("},
+          { text: "中華民國計算語言學會", link: "http://www.aclclp.org.tw/aclclp_organ.php"},
+          { text: "常務監事)"},
+        ]
+      },
+      {
+        time: "2021 - 2022",
+        content: [
+          { text: "Supervisor, Taiwan Association for Artificial Intelligence, TAAI (第十四屆 "},
+          { text: "中華民國人工智慧學會", link: "https://www.taai.org.tw/"},
+          { text: "常務監事)"},
+        ]
+      },
+      {
+        time: "2014 - 2018",
+        content: [
+          { text: "Committee Member, Research and Evaluation Commission, Taoyuan City Government (桃園市研考委員會委員)"},
+        ]
+      },
+      {
+        time: "2019.11 - 2021.10",
+        content: [
+          { text: "President, Association for Computational Linguistic and Chinese Language Processing, ACLCLP ("},
+          { text: "中華民國計算語言學會", link: "http://www.aclclp.org.tw/aclclp_organ.php"},
+          { text: "理事長)"},
+        ]
+      },
+      {
+        time: "2019 - 2020",
+        content: [
+          { text: "President, Taiwan Association for Artificial Intelligence, TAAI (第十三屆"},
+          { text: "中華民國人工智慧學會", link: "http://www.taai.org.tw/"},
+          { text: "理事長)"},
+        ]
+      },
+      {
+        time: "2017.11 - 2019.10",
+        content: [
+          { text: "Vice President, Association for Computational Linguistic and Chinese Language Processing, ACLCLP"},
+          { text: "中華民國計算語言學會", link: "http://www.aclclp.org.tw/aclclp_organ.php"},
+          { text: "理事長)"},
+        ]
+      },
+      {
+        time: "2018.7 - 2019.6",
+        content: [
+          { text: "Committee Member, Disaster prevention advisory committee (災害防救專家諮詢委員)"},
+        ]
+      },
+    ]
+  },
+  {
+    title: "Honors",
+    textArray: [
+      { time: "112-113學年度中央大學學術研究傑出獎" },
+      { time: "110學年度中央大學學術研究傑出獎" },
+      { time: "105-108學年度中央大學學術研究傑出獎" },
+      { time: "105年度中央大學績優技轉奬" },
+      { time: "105年度國科會優秀年輕學者研究計畫" },
+      { time: "99-105學年度中央大學學術研究傑出獎" },
+      { time: "100年度國科會優秀年輕學者研究計畫" },
+      { time: "99-100學年度中央大學學術研究傑出獎" },
+      { time: "92學年度中央大學資電學院新進研究優良教師獎" },
+      { time: "88年度國科會甲種奬勵" }
+    ]
+  },
+  {
+    title: "Education",
+    textArray: [
+      {
+        time: "1993.9 - 1999.1",
+        content: [
+          { text: "Ph.D. from"},
+          { text: "CSIE", link: "https://www.csie.ntu.edu.tw/"},
+          { text: ", "},
+          { text: "National Taiwan University", link: "https://www.ntu.edu.tw/"},
+          { text: "（台灣大學資工系博士）"},
+        ]
+      },
+      {
+        time: "1989.9 - 1993.6",
+        content: [
+          { text: "B.S. from the same department. Class"},
+          { text: "1993", link: "https://www.csie.ntu.edu.tw/~ntucs78/"},
+          { text: "（台灣大學資工系學士）"},
+        ]
+      },
+      {
+        time: "1986.9 - 1989.9",
+        content: [
+          { text: "at"},
+          { text: "LanYoung Girls High School", link: "https://www.lygsh.ilc.edu.tw/home"},
+          { text: "（蘭陽女中）"},
+        ]
+      },
+    ]
+  },
+  {
+    title: "Contacts",
+    textArray: [
+      {
+        time: "Office:",
+        content: [
+          { text: "Engineering Building 5", link: "https://www.ncu.edu.tw/images/campusmap.pdf"},
+          { text: ", B302"},
+        ]
+      },
+      {
+        time: "Email:",
+        content: [
+          { text: "chiahui@g.ncu.edu.tw"},
+        ]
+      },
+      {
+        time: "Tel:",
+        content: [
+          { text: "+886-3-422-7151 ext. 35302"},
+        ]
+      },
+      {
+        time: "Fax:",
+        content: [
+          { text: "+886-3-422-2681"},
+        ]
+      },
+    ]
   },
 ]
 
