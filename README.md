d9c35c9be1eea1c75bd924c8ef2ac73995fea017
黃懷萱 <chrbezz0487@gmail.com>
2024-05-20 02:56:35 +0800

    update the activate & readme

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ chmod +x run_docker.sh
 ./run_docker.sh
 ```
 
++ Generate the Swagger file.
+The file settings are in custom-templates.
+```
+openapi-generator-cli generate -i swagger.json -g javascript -o ./api-client -t ./custom-templates/javascript --additional-properties=usePromises=true
+```
--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 ﻿#!/bin/bash
 # sudo kill -9 $(sudo lsof -i :7230 | grep LISTEN | awk '{print $2}')
+# npm start
 
 docker build -t widm-front-end .
 docker run -d -p 7230:7230 -p 24678:24678 -v ${PWD}:/usr/src/app --name widm-front-end widm-front-end
 
-# npm start
-
 # chmod +x run_docker.sh
 # ./run_docker.sh
-
-# --legacy-peer-deps
 
 # openapi-generator-cli generate -i swagger.json -g javascript -o ./api-client -t ./custom-templates/javascript --additional-properties=usePromises=true
 
