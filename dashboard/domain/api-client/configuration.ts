99cb59d5fb715a2d9b21f463d22cd06938736d9f
黃懷萱 <56072039+Chrouos@users.noreply.github.com>
2024-05-26 01:18:36 +0800

    update dashboard api

--- dashboard/domain/api-client/configuration.ts
+++ dashboard/domain/api-client/configuration.ts
@@ -87,7 +87,7 @@ export class Configuration {
         this.username = param.username;
         this.password = param.password;
         this.accessToken = param.accessToken;
-        this.basePath = param.basePath ?? 'https://widm-back-end.nevercareu.space';
+        this.basePath = param.basePath;
         this.serverIndex = param.serverIndex;
         this.baseOptions = param.baseOptions;
         this.formDataCtor = param.formDataCtor;
